ccc24826cb5c6b969911c8e99a342903aaeff635
MarijaPaunovic <marijapaunovicdesign@gmail.com>
2022-10-11 15:27:46 +0200

    add react icons

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FormControl, Nav } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import './Header.css'
@@ -6,11 +7,19 @@ import './Header.css'
 function Header() {
     return (
         <div className='header'>
-            <Navbar bg="light" variant='light'>
+            <Navbar bg="dark" variant='dark'>
                 <Container>
                     <Navbar.Brand href="#home">
                         Online shop
                     </Navbar.Brand>
+                    <Navbar.Text className='search'>
+                        <FormControl
+                            style={{ width: 500 }}
+                            placeholder='Search a product'
+                            className='m-auto'
+                        />
+                    </Navbar.Text>
+
                 </Container>
             </Navbar>
         </div>
