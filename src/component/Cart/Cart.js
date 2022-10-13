a1170c3c3a764d0165448fa4d427e848d74fbd5e
MarijaPaunovic <marijapaunovicdesign@gmail.com>
2022-10-13 15:35:15 +0200

    add cart component

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,8 +1,96 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button, Col, Form, Image, ListGroup, Row } from "react-bootstrap";
+import { AiFillDelete } from "react-icons/ai";
+import { CartState } from "../../Context/Context";
+import "./Cart.css";
 
 const Cart = () => {
+    const [total, setTotal] = useState();
+
+    const options = [
+        { value: 1, text: 1 },
+        { value: 2, text: 2 },
+        { value: 3, text: 3 },
+        { value: 4, text: 4 },
+        { value: 5, text: 5 },
+    ];
+
+    const {
+        state: { cart },
+        dispatch,
+    } = CartState();
+
+    useEffect(() => {
+        setTotal(
+            cart.reduce((acc, curr) => acc + Number(curr.price) * curr.qty, 0)
+        );
+    }, [cart]);
+
     return (
-        <div>Cart</div>
+        <div className="home">
+            <div className="productContainer">
+                <ListGroup>
+                    {cart.map((prod) => (
+                        <ListGroup.Item key={prod.id}>
+                            <Row>
+                                <Col md={2}>
+                                    {
+                                        prod.images && prod.images.slice(0, 1).map((image, i) => <Image src={image} alt={prod.name} key={i} fluid rounded />)
+                                    }
+                                </Col>
+                                <Col md={2}>
+                                    <span>{prod.name}</span>
+                                </Col>
+                                <Col md={2}>$ {prod.price}</Col>
+                                <Col md={2}>
+                                    <Form.Select
+                                        aria-label="Qty"
+                                        value={prod.qty}
+                                        onChange={(e) => {
+                                            dispatch({
+                                                type: "CHANGE_CART_QTY",
+                                                payload: {
+                                                    id: prod.id,
+                                                    qty: e.target.value,
+                                                },
+                                            });
+                                        }
+                                        }
+                                    >
+                                        {options.map(option => (
+                                            <option key={option.text} value={option.value} >
+                                                {option.text}
+                                            </option>
+                                        ))}
+                                    </Form.Select>
+                                </Col>
+                                <Col md={2}>
+                                    <Button
+                                        type="button"
+                                        variant="light"
+                                        onClick={() =>
+                                            dispatch({
+                                                type: "REMOVE_FROM_CART",
+                                                payload: prod,
+                                            })
+                                        }
+                                    >
+                                        <AiFillDelete fontSize="20px" />
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
+            </div>
+            <div className="filters summary">
+                <span className="title">Subtotal ({cart.length}) items</span>
+                <span style={{ fontWeight: 700, fontSize: 20 }}>Total: $ {total}</span>
+                <Button type="button" disabled={cart.length === 0}>
+                    Proceed to Checkout
+                </Button>
+            </div>
+        </div>
     )
 }
 
